65db3d0347f956f5403bffad50ab8d9068327635
Jordan Capa <jcapasix@everis.com>
2019-06-16 10:32:51 -0500

    Se implemento la estructura inicial del proyecto

---
+++ Shareapp/Data/Network/Configuration.swift
@@ -0,0 +1,9 @@
+//
+//  Configuration.swift
+//  Shareapp
+//
+//  Created by Jordan Capa on 6/16/19.
+//  Copyright © 2019 Jordan Capa. All rights reserved.
+//
+
+import Foundation
---
+++ Shareapp/Presentation/Scenes/Login/LoginRouter/LoginRouter.swift
@@ -0,0 +1,9 @@
+//
+//  LoginRouter.swift
+//  Shareapp
+//
+//  Created by Jordan Capa on 6/16/19.
+//  Copyright © 2019 Jordan Capa. All rights reserved.
+//
+
+import Foundation
